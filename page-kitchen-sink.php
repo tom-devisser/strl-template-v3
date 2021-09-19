b880d644b88d16bda2160fbc73cd2a98bce7ff68
tomdevisser <tom.devisser@mail.com>
2021-09-19 16:06:49 +0200

    Made a kitchen sink page to display all components

---
+++ page-kitchen-sink.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package strl
+ * @since 1.0.0
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+	<div class="grid-container">
+		<div class="grid-x grid-padding-x grid-padding-y">
+			<div class="cell medium-6">
+				<h2>Heading</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<a href="#" class="btn-primary">Buy now</a>
+				<a href="#" class="btn-secondary">Contact Us</a>
+			</div>
+
+			<div class="cell medium-6">
+				<h2>Heading</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		</div</div><!-- .grid-container -->
+	</div><!-- .grid-x -->
+</main><!-- #primary -->
+
+<?php
+get_footer();
