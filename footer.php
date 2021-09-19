9732f3c657066ee32a9960ad020e13ba8ed28192
tomdevisser <tom.devisser@mail.com>
2021-09-20 01:06:19 +0200

    Added Footer widget areas

--- footer.php
+++ footer.php
@@ -13,8 +13,27 @@
 ?>
 
 	<footer class="site-footer">
-		<div class="grid-container site-footer-inner">
-			<p class="copyright">&copy; Stuurlui 2021</p>
+		<div class="grid-container">
+			<div class="grid-x grid-padding-x grid-padding-y">
+				<div class="cell medium-6 large-3">
+					<?php dynamic_sidebar( 'footer-1' ); ?>
+				</div>
+				<div class="cell medium-6 large-3">
+					<?php dynamic_sidebar( 'footer-2' ); ?>
+				</div>
+				<div class="cell medium-6 large-3">
+					<?php dynamic_sidebar( 'footer-3' ); ?>
+				</div>
+				<div class="cell medium-6 large-3">
+					<?php dynamic_sidebar( 'footer-4' ); ?>
+				</div>
+			</div>
+
+			<div class="grid-x site-footer-inner">
+				<div class="cell medium-6">
+					<p class="copyright">&copy; Stuurlui 2021</p>
+				</div>
+			</div>
 		</div>
 	</footer>
 </div><!-- #page .site -->
