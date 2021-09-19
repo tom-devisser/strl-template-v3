ea68da3da557fb8858fe342b4a59c14f2ff3c9a7
tomdevisser <tom.devisser@mail.com>
2021-09-19 12:08:24 +0200

    Added Font Awesome 5

--- functions.php
+++ functions.php
@@ -135,6 +135,8 @@ function strl_scripts() {
 	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/foundation.min.js', array( 'vendor' ), time(), true );
 	wp_enqueue_script( 'strl-scripts', get_template_directory_uri() . '/assets/scripts.min.js', array( 'foundation' ), time(), true );
 
+	wp_enqueue_script( 'font-awesome', 'https://kit.fontawesome.com/065b89fc32.js', array(), time(), false );
+
 	wp_enqueue_style( 'google-font-ubuntu', 'https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap', array(), '1.0.0' );
 	wp_enqueue_style( 'strl-styles', get_template_directory_uri() . '/assets/styles.min.css', array(), time() );
 }
--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 				<?php
 				if ( is_front_page() && is_home() ) {
 					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?> <i class="fab fa-wordpress-simple"></i></a></h1>
 					<?php
 				} else {
 					?>
