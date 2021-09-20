f424fd818c1a93646c970d8033644e4120018c29
tomdevisser <tom.devisser@mail.com>
2021-09-20 21:22:50 +0200

    Added breadcrumbs from NavXT

--- blocks/header/header.php
+++ blocks/header/header.php
@@ -18,6 +18,9 @@ $header_subtitle = get_sub_field( $prefix . 'subtitle' );
 <div class="grid-container full <?php echo esc_textarea( $blockname ); ?>-section">
 	<div class="grid-x grid-padding-y">
 		<div class="cell">
+			<div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
+				<?php function_exists( 'bcn_display' ) ? bcn_display() : ''; ?>
+			</div>
 			<h1><?php echo esc_textarea( $header_title ); ?></h1>
 		<?php
 		if ( isset( $header_subtitle ) ) {
