3e3c0306a2a4e06e7f8ec0abab309a2d322f3cb1
tomdevisser <tom.devisser@mail.com>
2021-09-21 14:48:10 +0200

    Refactored the Custom Shortcodes Button code

--- src/js/backend/tinymce-plugins/custom-shortcodes-button.js
+++ src/js/backend/tinymce-plugins/custom-shortcodes-button.js
@@ -6,80 +6,86 @@
 		init( editor, url ) {
 			url = url.replace( '/js', '' );
 
+			// The Button Object to add to the STRL Shortcodes menu.
+			const buttonObject = {
+				text: 'Button',
+				onclick() {
+					editor.windowManager.open( {
+						width: 700,
+						height: 400,
+						title: 'Button',
+						body: [ {
+							type: 'textbox',
+							id: 'button-text',
+							name: 'text',
+							label: 'Text',
+							value: '',
+						}, {
+							type: 'textbox',
+							id: 'button-url',
+							name: 'url',
+							label: 'Link',
+							value: '',
+						}, {
+							type: 'listbox',
+							id: 'button-target',
+							name: 'target',
+							label: 'Target',
+							values: [
+								{ value: 'self', text: 'Current window (recommended for internal links)' },
+								{ value: 'blank', text: 'Open in a new tab (recommended for external links)' },
+							],
+						}, {
+							type: 'listbox',
+							id: 'button-type',
+							name: 'type',
+							label: 'Type',
+							values: [
+								{ value: 'primary', text: 'Primary' },
+								{ value: 'secondary', text: 'Secondary' },
+							],
+						} ],
+						onsubmit( window ) {
+							editor.insertContent( '[button url="' + window.data.url + '" target="' + window.data.target + '" type="' + window.data.type + '"]' + window.data.text + '[/button]' );
+						},
+					} );
+				},
+			};
+
+			// The Accordion Object to add to the STRL Shortcodes menu.
+			const accordionObject = {
+				text: 'Accordion',
+				onclick() {
+					editor.windowManager.open( {
+						width: 700,
+						height: 400,
+						title: 'Accordion',
+						body: [ {
+							type: 'textbox',
+							id: 'accordion-title',
+							name: 'title',
+							label: 'Title',
+							value: '',
+						}, {
+							type: 'textbox',
+							id: 'accordion-text',
+							name: 'text',
+							label: 'Text',
+							value: '',
+						} ],
+						onsubmit( window ) {
+							editor.insertContent( '[accordion title="' + window.data.title + '"]' + window.data.text + '[/accordion]' );
+						},
+					} );
+				},
+			};
+
 			// Creates and adds custom buttons to TinyMCE Editor
 			editor.addButton( 'strl_shortcodes', {
 				title: 'STRL',
 				image: url + '/img/icon-STRL-blauw.svg',
 				type: 'menubutton',
-				menu: [ {
-					text: 'Button',
-					onclick() {
-						editor.windowManager.open( {
-							width: 700,
-							height: 400,
-							title: 'Button',
-							body: [ {
-								type: 'textbox',
-								id: 'button-text',
-								name: 'text',
-								label: 'Text',
-								value: '',
-							}, {
-								type: 'textbox',
-								id: 'button-url',
-								name: 'url',
-								label: 'Link',
-								value: '',
-							}, {
-								type: 'listbox',
-								id: 'button-target',
-								name: 'target',
-								label: 'Target',
-								values: [
-									{ value: 'self', text: 'Current window (recommended for internal links)' },
-									{ value: 'blank', text: 'Open in a new tab (recommended for external links)' },
-								],
-							}, {
-								type: 'listbox',
-								id: 'button-type',
-								name: 'type',
-								label: 'Type',
-								values: [
-									{ value: 'primary', text: 'Primary' },
-									{ value: 'secondary', text: 'Secondary' },
-								],
-							} ],
-							onsubmit( window ) {
-								editor.insertContent( '[button url="' + window.data.url + '" target="' + window.data.target + '" type="' + window.data.type + '"]' + window.data.text + '[/button]' );
-							},
-						} );
-					},
-				}, {
-					text: 'Accordion',
-					onclick() {
-						editor.windowManager.open( {
-							width: 700,
-							height: 400,
-							title: 'Accordion',
-							body: [ {
-								type: 'textbox',
-								id: 'accordion-title',
-								name: 'title',
-								label: 'Title',
-								value: '',
-							}, {
-								type: 'textbox',
-								id: 'accordion-text',
-								name: 'text',
-								label: 'Text',
-								value: '',
-							} ],
-							onsubmit( window ) {
-								editor.insertContent( '[accordion title="' + window.data.title + '"]' + window.data.text + '[/accordion]' );
-							},
-						} );
-					},
-				} ],
+				menu: [ buttonObject, accordionObject ],
 			} );
 		},
 	} );
