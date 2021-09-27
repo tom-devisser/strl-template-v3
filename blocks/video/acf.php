000712313f183978289c65ef7f8ff1af682517bd
tomdevisser <tom.devisser@mail.com>
2021-09-27 21:19:06 +0200

    Added the video pop-up option in a Foundation Reveal

--- blocks/video/acf.php
+++ blocks/video/acf.php
@@ -29,6 +29,22 @@ $blocks[ $block_name ] = array(
 			'required'     => 1,
 		),
 		array(
+			'key'               => $block_name . '-video-thumbnail',
+			'name'              => $block_name . '-video-thumbnail',
+			'label'             => __( 'Video Thumbnail', 'strl' ),
+			'instructions'      => __( 'The image that shows before you click the play button. This only shows up when you\'ve selected pop-up in Video Settings.', 'strl' ),
+			'type'              => 'image',
+			'conditional_logic' => array(
+				array(
+					array(
+						'field'    => $block_name . '-pop-up',
+						'operator' => '==',
+						'value'    => 'yes',
+					),
+				),
+			),
+		),
+		array(
 			'key'   => $block_name . '-settings-tab',
 			'label' => __( 'Video Settings', 'strl' ),
 			'name'  => $block_name . '-settings-tab',
@@ -42,6 +58,18 @@ $blocks[ $block_name ] = array(
 			'choices'       => array(
 				'yes' => __( 'Yes, I want this row full width', 'strl' ),
 				'no'  => __( 'No, I want this row within the container', 'strl' ),
+			),
+			'required'      => 1,
+			'default_value' => 'no',
+		),
+		array(
+			'key'           => $block_name . '-pop-up',
+			'label'         => __( 'Video Pop-Up', 'strl' ),
+			'name'          => $block_name . '-pop-up',
+			'type'          => 'radio',
+			'choices'       => array(
+				'yes' => __( 'Yes, I want this video in a pop-up', 'strl' ),
+				'no'  => __( 'No, I want this video directly on the page', 'strl' ),
 			),
 			'required'      => 1,
 			'default_value' => 'no',
--- blocks/video/video.php
+++ blocks/video/video.php
@@ -12,14 +12,45 @@ $prefix    = $blockname . '-';
 // Block Field Values.
 $video_id   = get_sub_field( $prefix . 'video-id' );
 $full_width = get_sub_field( $prefix . 'full-width' );
+$pop_up     = get_sub_field( $prefix . 'pop-up' );
 ?>
 <!-- <?php echo esc_textarea( $blockname ); ?> -->
 <div class="grid-container <?php echo esc_textarea( $blockname ); ?>-section<?php echo ( 'yes' === $full_width ? ' full' : '' ); ?>">
 	<div class="grid-x grid-padding-y<?php echo ( 'yes' === $full_width ? '' : 'grid-padding-x' ); ?>">
 		<div class="cell <?php echo esc_textarea( $prefix ) . 'column'; ?>">
-			<div class="video-container">
-				<div class="video" id="video-<?php echo esc_textarea( $video_id ); ?>" data-video-id="<?php echo esc_textarea( $video_id ); ?>"></div>
-			</div>
+			<?php
+			if ( 'no' === $pop_up ) {
+				?>
+				<div class="video-container">
+					<div class="video" id="video-<?php echo esc_textarea( $video_id ); ?>" data-video-id="<?php echo esc_textarea( $video_id ); ?>"></div>
+				</div>
+				<?php
+			} elseif ( 'yes' === $pop_up ) {
+				$thumbnail = get_sub_field( $prefix . 'video-thumbnail' );
+				?>
+				<div class="video-preview" style="background-image: url('<?php echo esc_url( $thumbnail['url'] ); ?>')">
+					<button class="play-button" data-open="reveal-video-<?php echo esc_textarea( $video_id ); ?>">
+						<i class="fas fa-play"></i>
+						<span class="screen-reader-text"><?php esc_html_e( 'Open video pop-up.', 'strl' ); ?></span>
+					</button>
+				</div>
+
+				<!-- Video Pop-Up -->
+				<div class="reveal small" id="reveal-video-<?php echo esc_textarea( $video_id ); ?>" data-reveal>
+					<div class="video-container">
+						<div class="video" id="video-<?php echo esc_textarea( $video_id ); ?>" data-video-id="<?php echo esc_textarea( $video_id ); ?>"></div>
+					</div>
+
+					<!-- Close reveal -->
+					<button class="close-button" data-close aria-label="Close modal" type="button">
+						<span aria-hidden="true">&times
+							<span class="screen-reader-text"><?php esc_html_e( 'Close video pop-up.', 'strl' ); ?></span>
+						</span>
+					</button>
+				</div>
+				<?php
+			}
+			?>
 		</div>
 	</div>
 </div>
